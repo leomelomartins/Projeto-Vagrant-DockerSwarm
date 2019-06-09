f47d8310a5c1135622f024f61e13bd19e356201a
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-09 18:40:10 -0300

    best bet

--- client_config.sh
+++ client_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd Projeto-Vagrant-DockerSwarm/containerClient
-chmod +x /vagrant/containerClient/token.sh
-bash /vagrant/containerClient/token.sh
+chmod +x /vagrant/token.sh
+bash /vagrant/token.sh
 sudo docker build -t client .
