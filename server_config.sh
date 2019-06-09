410145657128cdc280ecc32e815380faf1632aa6
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-09 16:02:43 -0300

    VAI

--- server_config.sh
+++ server_config.sh
@@ -4,4 +4,4 @@ cd Projeto-Vagrant-DockerSwarm/containerServer
 sudo docker swarm init --advertise-addr 192.168.50.2:2377 | sed 'd' > /../containerClient/token.sh
 sudo docker build -t server .
 sudo docker network create -d overlay --subnet 10.0.10.0/24 ClusterNet
-sudo doocker service create -d server_on --network ClusterNet --replicas 3 -p 5000:80 server
+sudo docker service create -d server_on --network ClusterNet --replicas 3 -p 5000:80 server
