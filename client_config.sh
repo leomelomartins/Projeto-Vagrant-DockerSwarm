6a2a7620b2faf9ec68abcd3f967808990ab82f39
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-09 14:49:49 -0300

    testing

--- client_config.sh
+++ client_config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt install -y python3 python3-pip
 pip3 install bottle docker
-cd containerClient
+cd Projeto-Vagrant-DockerSwarm/containerClient
 chmod +x token.sh
 bash token.sh
 sudo docker build -t client .
--- server_config.sh
+++ server_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd containerServer
+cd Projeto-Vagrant-DockerSwarm/containerServer
 sudo docker swarm init --advertise-addr 192.168.50.2:2377 | sed 'd' > token.sh
 sudo docker build -t server .
 sudo docker network create -d overlay --subnet 10.0.10.0/24 ClusterNet
