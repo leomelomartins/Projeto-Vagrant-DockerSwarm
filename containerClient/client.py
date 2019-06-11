79e3077b632b043a26eab2ba522b700b94777626
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-11 03:58:44 -0300

    é isso.

--- containerClient/client.py
+++ containerClient/client.py
@@ -2,21 +2,20 @@ import requests
 import json
 import docker
 
-print('==============================================')
 print('Escolha uma das seguintes opções: ')
-print('1 - Armazenar dados do container.')
-print('2 - Receber dados do servidor.')
-print('9 - Sair da aplicação.')
-print('==============================================')
+print('1 - Armazenar dados do container. [POST]')
+print('2 - Receber dados do servidor.[GET]')
+print('0 - Sair da aplicação.')
+print('\n')
 
 x = input()
 
 if x == 1:
     cli = docker.from_env()
     print(cli)
-    stats = cli.stats('containercliente')
+    stats = cli.stats('containerWorker')
     print(stats)
-    url = 'http://192.168.50.2:5000/container'
+    url = 'http://192.168.50.2:5000/POST'
     for stat in stats:
         # TODO
         payload = {
@@ -29,9 +28,11 @@ if x == 1:
         }
     headers = {'content-type': 'application/json'}
     r = requests.post(url, data=json.dumps(payload), headers=headers)
+
 if x == 2:
-    url = 'http://192.168.50.2:5000/containers'
+    url = 'http://192.168.50.3:5000/GET'
     r = requests.get(url)
     print(r)
-if x == 9:
+
+if x == 0:
     exit()
--- containerServer/server.py
+++ containerServer/server.py
@@ -2,11 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
-@app.route('/container', methods=['POST'])
-def container():
+@app.route('/POST')
+def GET():
     content = request.json
-    file = open('containers.txt', 'w')
+    file = open('logs.txt', 'w')
 
     keys = content.keys()
     for key in keys:
@@ -14,16 +13,15 @@ def container():
         file.write(aux)
     file.write('\n')
 
-    data = {'message': 'Dados do container armazenados com sucesso!'}
+    data = {'message': 'Dados do container armazenados!'}
     return jsonify(data), 201
 
 
-@app.route('/containers', methods=['GET'])
-def containers():
-    file = open('containers.txt')
+@app.route('/GET')
+def GET():
+    file = open('logs.txt')
     return file.read(), 200
 
 
 if __name__ == '__main__':
     app.run()
-
