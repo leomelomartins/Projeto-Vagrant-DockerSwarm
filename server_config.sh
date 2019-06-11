816e04d3d9a8a8f0211be1380ec3ca41b4769c90
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-11 00:23:41 -0300

    nao sei mais oq to fazendo

--- server_config.sh
+++ server_config.sh
@@ -3,6 +3,7 @@
 cd Projeto-Vagrant-DockerSwarm/containerServer
 
 #iniciando o DockerSwarm
+firewall-cmd --add-port=2377/tcp --permanent firewall-cmd --reload
 sudo docker swarm init --advertise-addr 192.168.50.2:2377 | sed 5!d > /vagrant/token.sh
 
 #Construindo imagem do Dockerfile do container Server
@@ -12,4 +13,4 @@ sudo docker build -t server .
 sudo docker network create -d overlay --subnet 10.0.10.0/24 ClusterNet
 
 #Criação do serviço
-sudo docker service create -d --name webserver --network ClusterNet --replicas 3 -p 5001:80 server
+sudo docker service create -d --name webservice --network ClusterNet --replicas 2 -p 5001:80 server
