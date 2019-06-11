6c012afb943099b27b4ca48d885e6b8050bb3eb9
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-11 02:56:38 -0300

    big

--- containerClient/client.py
+++ containerClient/client.py
@@ -1,10 +1,37 @@
-from flask import flask
-app = Flask(__name__)
+import requests
+import json
+import docker
 
-@app.route('/')
-def index():
-    return 'Server Works!'
+print('==============================================')
+print('Escolha uma das seguintes opções: ')
+print('1 - Armazenar dados do container.')
+print('2 - Receber dados do servidor.')
+print('9 - Sair da aplicação.')
+print('==============================================')
 
-@app.route('/greet')
-    def say_hello():
-        return 'Hello from Server!'
+x = input()
+
+if x == 1:
+    cli = docker.from_env()
+    print(cli)
+    stats = cli.stats('containercliente')
+    print(stats)
+    url = 'http://192.168.50.2:5000/container'
+    for stat in stats:
+        # TODO
+        payload = {
+            'Horario da Requisiçao': stat,
+            'Nome do Container': stat,
+            'Id do Container': stat,
+            'Id da Imagem': stat,
+            'Uso de CPU': stat,
+            'Uso de Memoria': stat
+        }
+    headers = {'content-type': 'application/json'}
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+if x == 2:
+    url = 'http://192.168.50.2:5000/containers'
+    r = requests.get(url)
+    print(r)
+if x == 9:
+    exit()
--- containerServer/server.py
+++ containerServer/server.py
@@ -1,10 +1,29 @@
-from flask import flask
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Server Works!'
 
-@app.route('/greet')
-    def say_hello():
-        return 'Hello from Server!'
+@app.route('/container', methods=['POST'])
+def container():
+    content = request.json
+    file = open('containers.txt', 'w')
+
+    keys = content.keys()
+    for key in keys:
+        aux = key + '->' + content[key] + ' '
+        file.write(aux)
+    file.write('\n')
+
+    data = {'message': 'Dados do container armazenados com sucesso!'}
+    return jsonify(data), 201
+
+
+@app.route('/containers', methods=['GET'])
+def containers():
+    file = open('containers.txt')
+    return file.read(), 200
+
+
+if __name__ == '__main__':
+    app.run()
+
