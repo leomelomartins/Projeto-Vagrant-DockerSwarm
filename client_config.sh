f1aec5ace10cb4870aa61d52ab0d360f96f576cd
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-10 22:42:49 -0300

    a

--- client_config.sh
+++ client_config.sh
@@ -6,6 +6,6 @@ sudo apt install -y python3-pip
 sudo pip3 install Flask
 
 cd Projeto-Vagrant-DockerSwarm/containerClient
-chmod +x /vagrant/token/join.sh
-bash /vagrant/token/join.sh
+chmod +x /vagrant/token.sh
+bash /vagrant/token.sh
 sudo docker build -t client .
