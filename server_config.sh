b64f0c5badab7ff88f3267a769df578cb7fc7bcb
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-10 22:02:28 -0300

    teste ultra foda

--- server_config.sh
+++ server_config.sh
@@ -3,7 +3,7 @@
 cd Projeto-Vagrant-DockerSwarm/containerServer
 
 #iniciando o DockerSwarm
-sudo docker swarm init --advertise-addr 192.168.50.2:2376 | sed 5!d > /vagrant/token/join.sh
+sudo docker swarm init --advertise-addr 192.168.50.2:2377 | sed 5!d > /vagrant/token/join.sh
 
 #Construindo imagem do Dockerfile do container Server
 sudo docker build -t server .
@@ -12,4 +12,4 @@ sudo docker build -t server .
 sudo docker network create -d overlay --subnet 10.0.10.0/24 ClusterNet
 
 #Criação do serviço
-sudo docker service create -d --name webserver --network ClusterNet --replicas 3 -p 2376:80 server
+sudo docker service create -d --name webserver --network ClusterNet --replicas 3 -p 5001:80 server
