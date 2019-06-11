da0bb31c15080b1aa028c03697d71372aa706fdb
leomelomartins <leonardo.melo.mar@gmail.com>
2019-06-11 00:32:04 -0300

    lll

--- server_config.sh
+++ server_config.sh
@@ -3,12 +3,8 @@
 cd Projeto-Vagrant-DockerSwarm/containerServer
 
 #iniciando o DockerSwarm
-<<<<<<< HEAD
 firewall-cmd --add-port=2377/tcp --permanent firewall-cmd --reload
 sudo docker swarm init --advertise-addr 192.168.50.2:2377 | sed 5!d > /vagrant/token.sh
-=======
-sudo docker swarm init --advertise-addr 192.168.50.2:7946 | sed 5!d > /vagrant/token.sh
->>>>>>> e379e2b89b4afba4f09941fd6a208af522c0246b
 
 #Construindo imagem do Dockerfile do container Server
 sudo docker build -t server .
@@ -17,8 +13,4 @@ sudo docker build -t server .
 sudo docker network create -d overlay --subnet 10.0.10.0/24 ClusterNet
 
 #Criação do serviço
-<<<<<<< HEAD
 sudo docker service create -d --name webservice01 --network ClusterNet --replicas 3 -p 5001:80 server
-=======
-sudo docker service create -d --name webservice --network ClusterNet --replicas 2 -p 5001:80 server
->>>>>>> e379e2b89b4afba4f09941fd6a208af522c0246b
